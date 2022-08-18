9aaa95096687140f39323317d1041c54b5cc4e74
Jgardin875 <jess.gardin88@gmail.com>
2022-08-18 18:08:10 -0400

    updated most files

--- analysis_p.py
+++ analysis_p.py
@@ -89,258 +89,61 @@ example = example.drop(columns = ['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainf
 example = example.resample('M').mean()
 
 
-# In[ ]:
-
-
-
-
-
-# # Stats Testing
-
-# # I have all continuous variables so I will use Spearman or Pearson
-# # Spearman is better with monotonic data so I choose that one
-
-# In[9]:
-
-
-cat_col = lake.columns
-
-
-# In[10]:
-
-
-stat_norm = []
-
-
-# In[11]:
-
-
-for col in cat_col:
-    stats.spearmanr(lake.Lake_Level, lake[col])
-    stat_norm.append(stats.spearmanr(lake.Lake_Level, lake[col]))
-
-
-# In[12]:
-
-
-stat_norm = pd.DataFrame(stat_norm, index = cat_col).round(4)
-
-
-# In[13]:
-
-
-stat_norm
-
-
-# # 3 Month Shift
-# 3 months is appoximately 90 days
-
-# In[14]:
-
-
-lake_s = acquire_p.get_bilancino_data()
-
-
-# In[15]:
-
-
-lake_s = acquire_p.prepare(lake_s)
-
-
-# In[16]:
-
-
-lake_s[['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata',
-       'Rainfall_Cavallina', 'Rainfall_Le_Croci', 'Temperature_Le_Croci']] = lake_s[['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata',
-       'Rainfall_Cavallina', 'Rainfall_Le_Croci', 'Temperature_Le_Croci']].shift(periods = 90, fill_value=0)
-
-
-# In[17]:
-
-
-lake_s.resample('M').sum()
-
-
-# In[18]:
-
-
-lake.resample('M').sum()
-
-
-# In[19]:
-
-
-lake_s.head(91)
-
-
-# In[20]:
-
-
-lake_s = lake_s[lake_s.index >= '2005-04-01']
-
-
-# In[21]:
-
-
-cat_col = lake_s.columns
-
-
-# In[22]:
-
-
-stat_3m_s = []
-
-
-# In[23]:
-
-
-for col in cat_col:
-    stats.spearmanr(lake_s.Lake_Level, lake_s[col])
-    stat_3m_s.append(stats.spearmanr(lake_s.Lake_Level, lake_s[col]))
-
-
-# In[24]:
-
-
-stat_3m_s = pd.DataFrame(stat_3m_s, index = cat_col, columns = ['corr_3m_s', 'p_3m_s']).round(4)
-
-
-# In[25]:
-
-
-stat_3m_s
-
-
-# # Compare
-
-# In[26]:
-
-
-stat_sum = stat_norm.join(stat_3m_s)
-
-
-# In[27]:
-
-
-stat_sum
-
-
 # In[28]:
 
 
-stat_sum_cor = stat_sum[['correlation', 'corr_3m_s']]
+lake.columns
 
 
 # In[29]:
 
 
-stat_sum_cor = stat_sum_cor.abs()
+def spearmanr(df):
+    spear = []
+    cat_col = ['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata',
+       'Rainfall_Cavallina', 'Rainfall_Le_Croci', 'Temperature_Le_Croci',
+       'Lake_Level', 'Flow_Rate']
+    for col in cat_col:
+        stats.spearmanr(df.Lake_Level, df[col])
+        spear.append(stats.spearmanr(df.Lake_Level, df[col]))
+    l = pd.DataFrame(spear, index = cat_col).round(4)
+    return l
 
 
 # In[30]:
 
 
-stat_sum_cor = stat_sum_cor.drop(index = ['Flow_Rate', 'Lake_Level'])
+def spearmanr_a(df):
+    spear = []
+    cat_col = ['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata',
+       'Rainfall_Cavallina', 'Rainfall_Le_Croci', 'Temperature_Le_Croci',
+       'Lake_Level', 'Flow_Rate', 'avg_rain']
+    for col in cat_col:
+        stats.spearmanr(df.Lake_Level, df[col])
+        spear.append(stats.spearmanr(df.Lake_Level, df[col]))
+    l = pd.DataFrame(spear, index = cat_col).round(4)
+    return l
 
 
-# In[31]:
+# # 3 Month Shift
+# 3 months is appoximately 90 days
+
+# In[27]:
 
 
-#spear_cor = stat_sum_cor.plot(kind = 'barh', title = 'Strenght of Correlation to Lake Level')
+def shift90(df):
+    col_list = ['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata', 'Rainfall_Cavallina', 'Rainfall_Le_Croci', 'Temperature_Le_Croci']
+    df[col_list] = df[col_list].shift(periods = 90, fill_value = 0)
+    df = df[df.index >= '2005-04-01']
+    return df
 
+
+# # Model
 
 # In[32]:
 
 
-#plt.savefig('spear_cor.png')
-
-
-# In[33]:
-
-
-stat_sum_p = stat_sum[['pvalue', 'p_3m_s']]
-
-
-# In[34]:
-
-
-stat_sum_p = stat_sum_p.drop(index = ['Flow_Rate', 'Lake_Level'])
-
-
-# In[35]:
-
-
-#spear_p = stat_sum_p.plot(kind = 'barh', title = 'Statisitcal Likelihood of Random Correlation')
-
-
-# In[36]:
-
-
-#plt.savefig('spear_p.png')
-
-
-# In[37]:
-
-
-mod_col = lake_s.drop(columns = ['Rainfall_S_Piero', 'Rainfall_Mangona', 'Rainfall_S_Agata',
-       'Rainfall_Cavallina', 'Rainfall_Le_Croci'])
-
-
-# In[38]:
-
-
-mod_col = mod_col.resample('M').mean()
-
-
-# In[39]:
-
-
-train = mod_col[mod_col.index < '2014-01-01']
-validate = mod_col[(mod_col.index >= '2014-01-01') & (mod_col.index < '2018-01-01')]
-test = mod_col[mod_col.index >= '2018-01-01']
-
-
-# In[40]:
-
-
-trainx = train.drop(columns = 'Lake_Level')
-trainy = train.Lake_Level
-
-validatex = validate.drop(columns = 'Lake_Level')
-validatey = validate.Lake_Level
-
-testx = test.drop(columns = 'Lake_Level')
-testy = test.Lake_Level
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[41]:
-
-
-def evaluate(target_var):
+def evaluate(target_var, yhat_df, validate):
     '''
     This function will take the actual values of the target_var from validate, 
     and the predicted values stored in yhat_df, 
@@ -351,10 +154,10 @@ def evaluate(target_var):
     return rmse
 
 
-# In[42]:
+# In[34]:
 
 
-def plot_and_eval(target_var):
+def plot_and_eval(target_var, train, yhat_df, validate):
     '''
     This function takes in the target var name (string), and returns a plot
     of the values of train for that variable, validate, and the predicted values from yhat_df. 
@@ -365,25 +168,201 @@ def plot_and_eval(target_var):
     plt.plot(validate[target_var], label='Validate', linewidth=1)
     plt.plot(yhat_df[target_var])
     plt.title(target_var)
-    rmse = evaluate(target_var)
+    rmse = evaluate(target_var, yhat_df, validate)
     print(rmse)
     plt.show()
+    return rmse
 
 
-# In[43]:
+# In[26]:
 
 
 # function to store the rmse so that we can compare
-def append_eval_df(model_type, target_var):
+def append_eval_df(model_type, target_var, yhat_df, validate):
     '''
     this function takes in as arguments the type of model run, and the name of the target variable. 
     It returns the eval_df with the rmse appended to it for that model and target_var. 
     '''
-    rmse = evaluate(target_var)
+    rmse = evaluate(target_var, yhat_df, validate)
     d = {'model_type': [model_type],
         'rmse': [rmse]}
     d = pd.DataFrame(d)
     return eval_df.append(d, ignore_index = True)
+
+
+# In[35]:
+
+
+eval_df = pd.DataFrame(columns=['model_type', 'rmse'])
+eval_df
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def polynomial_transform(x_tr_data, y_tr_data, x_val_data, y_val_data):
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=2)
+
+    # fit and transform X_train_scaled ONLY training gets fit, even for learning transformation!!!
+    x_tr_data_deg2 = pf.fit_transform(x_tr_data)
+
+    # transform X_validate_scaled & X_test_scaled
+    x_val_data_deg2 = pf.transform(x_val_data)
+
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data
+    lm2.fit(x_tr_data_deg2, y_tr_data)
+
+    # predict train
+    y_tr_data_deg2 = lm2.predict(x_tr_data_deg2)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_tr_data, y_tr_data_deg2)**(1/2)
+
+    # predict validate
+    y_val_data_deg2 = lm2.predict(x_val_data_deg2)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_val_data, y_val_data_deg2)**(1/2)
+
+    print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train.round(2), 
+          "\nValidation/Out-of-Sample: ", rmse_validate.round(2))
+
+    return y_tr_data_deg2, y_val_data_deg2 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+def glm(x_tr_data, y_tr_data, x_val_data, y_val_data):
+
+    # create the model object
+    lm = TweedieRegressor(power=1, alpha=0)
+
+    # fit the model ONLY to our training data.  
+
+    lm.fit(x_tr_data, y_tr_data)
+
+    # predict train
+    y_tr_predict_glm = lm.predict(x_tr_data)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_tr_data, y_tr_predict_glm)**(1/2)
+
+    # predict validate
+    y_val_predict_glm = lm.predict(x_val_data)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_val_data, y_val_predict_glm)**(1/2)
+
+    print("RMSE for OLS using GLM\nTraining/In-Sample: ", rmse_train.round(2), 
+          "\nValidation/Out-of-Sample: ", rmse_validate.round(2))
+    return y_tr_predict_glm, y_val_predict_glm
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # Test
+
+# In[ ]:
+
+
+def evaluate_test(target_var, yhat_df, test):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    rmse = round(sqrt(mean_squared_error(test[target_var], yhat_df[target_var])), 4)
+    return rmse
+
+
+# In[ ]:
+
+
+def plot_and_eval_test(target_var, train, validate, test, yhat_df):
+    '''
+    This function takes in the target var name (string), and returns a plot
+    of the values of train for that variable, validate, and the predicted values from yhat_df. 
+    it will als lable the rmse. 
+    '''
+    plt.figure(figsize = (12,4))
+    plt.plot(train[target_var], label='Train', linewidth=1)
+    plt.plot(validate[target_var], label='Validate', linewidth=1)
+    plt.plot(test[target_var], label='Test', linewidth=1)
+    plt.plot(yhat_df[target_var])
+    plt.title(target_var)
+    rmse = evaluate(target_var, yhat_df, test)
+    print(rmse)
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
